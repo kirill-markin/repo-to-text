2a08a70cf48eac60c2b27e2e0c64b55805a8439d
Kirill Markin <markinkirill@gmail.com>
2024-12-17 17:06:16 +0100

    linter cleanup

--- repo_to_text/core/__init__.py
+++ repo_to_text/core/__init__.py
@@ -1,3 +1,5 @@
+"""This module contains the core functionality of the repo_to_text package."""
+
 from .core import get_tree_structure, load_ignore_specs, should_ignore_file, save_repo_to_text
 
 __all__ = ['get_tree_structure', 'load_ignore_specs', 'should_ignore_file', 'save_repo_to_text'] 
